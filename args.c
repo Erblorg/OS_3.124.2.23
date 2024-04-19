f64d7589a000bb0d9ebd11ed2ab5bf6cf8f27338
Трубачеев А.Д <trubacheev260105@gmail.com>
2024-04-19 11:22:28 +0700

    Изменили поведение программы с аргументами командной строки

--- args.c
+++ args.c
@@ -3,14 +3,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 int main(int args, char *argv[], char *envp[]) {
-	printf("Количество параметров программы %d\n", args);
-	for (int i=0;i<args; i++) {
-		printf("Параметр %d: %s\n", i, argv[i]);
+	if(args>1) {
+		printf("Количество параметров программы %d\n", args);
+		for (int i=0;i<args; i++) {
+			printf("Параметр %d: %s\n", i, argv[i]);
+		}
 	}
-	int j=0;
-	while (envp[j]!=NULL) {
-		printf("Значение параметра %d: %s\n", j, envp[j]);
-		j++;
+	else {
+		int j=0;
+		while (envp[j]!=NULL) {
+			printf("Значение параметра %d: %s\n", j, envp[j]);
+			j++;
+		}
 	}
 	return 0;
 }
